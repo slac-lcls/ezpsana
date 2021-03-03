6ddd95e57a9e4707f813aafe91991bae3f11ec6b
fredericpoitevin <frederic.poitevin@gmail.com>
2021-03-03 11:44:30 -0800

    updated README and sync-yaml.sh in conda to reflect the latest changes from Valerio

--- conda/sync-yaml.sh
+++ conda/sync-yaml.sh
@@ -7,13 +7,6 @@ for yml in "ana-${version}.yml" "ana-${version}-py3.yml"; do
 
   echo "...> scp ${USER}@psexport.slac.stanford.edu:${pslocation}${yml} ${yml}.template"
   scp ${USER}@psexport.slac.stanford.edu:${pslocation}${yml} ${yml}.template
-
-  #echo "...> removing local channel line in yaml file"
-  #grep -v "file:///" tmp.yaml > ana-env-${py}.yaml.template; rm -f tmp.yaml
-  #
-  #echo "...> changing env name in yaml file to ana-env-${py}"
-  #sed -i "s/name: ana/name: ana-${py}/g" ana-env-${py}.yaml.template
-
   echo "!!!> checkout template file: ${yml}.template"
 
 done
