630100455cfe30ddb3aca0c4692ddf854311387d
fredericpoitevin <frederic.poitevin@gmail.com>
2021-04-20 13:46:00 -0700

    sync-yaml.sh now rsync the whole envfiles

--- sync-yaml.sh
+++ sync-yaml.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-version=${1-latest}
 pslocation="/cds/sw/ds/ana/conda1/envfiles/"
 
-for yml in "ana-${version}.yml" "ana-${version}-py3.yml"; do
+rsync -avr ${USER}@psexport.slac.stanford.edu:${pslocation} ./yaml
 
-  echo "...> scp ${USER}@psexport.slac.stanford.edu:${pslocation}${yml} "
-  scp ${USER}@psexport.slac.stanford.edu:${pslocation}${yml} yaml/${yml}
-  echo "!!!> checkout template file: yaml/${yml}"
-
-done
