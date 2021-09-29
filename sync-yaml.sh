1f54d8d2aa21d9bd13532688437638c5d97054e5
Valerio Mariani <valmar@slac.stanford.edu>
2021-09-28 17:21:32 -0700

    Updated sync script for moved directory

--- sync-yaml.sh
+++ sync-yaml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pslocation="/cds/sw/ds/ana/conda1/envfiles/"
+pslocation="/cds/sw/ds/ana/conda1/manage/envfiles/"
 
 rsync -avr ${USER}@psexport.slac.stanford.edu:${pslocation} ./yaml
 
