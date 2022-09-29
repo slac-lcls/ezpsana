c51535c310318ec4c33df75aeb0a5a5ed3c46a5a
fredericpoitevin <frederic.poitevin@gmail.com>
2022-09-29 16:13:24 -0700

    psana2

--- docker/psana2/entrypoint.sh
+++ docker/psana2/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+unset LD_LIBRARY_PATH
+unset PYTHONPATH
+
 source /opt/anaconda/etc/profile.d/conda.sh
 conda activate ana
-export PATH=/home/btx-build/btx/scripts:$PATH
+export PATH=/home/psana2-build/lcls2/install/bin:$PATH
+export PATH=/home/psana-build/btx/scripts:$PATH
+export PYTHONPATH=/home/psana2-build/lcls2/install/lib/python3.10/site-packages
+export PYTHONPATH=/home/psana2-build/btx:$PYTHONPATH
 
 exec "$@"
