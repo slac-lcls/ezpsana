1368c4c52fbfe9b60286f863d2547809ecb1addd
fredericpoitevin <frederic.poitevin@gmail.com>
2022-10-28 09:58:37 -0700

    python version 3.9 not 3.10

--- docker/psana2-nersc/entrypoint.sh
+++ docker/psana2-nersc/entrypoint.sh
@@ -7,7 +7,7 @@ source /opt/anaconda3/etc/profile.d/conda.sh
 conda activate ana
 export PATH=/home/psana2-build/lcls2/install/bin:$PATH
 export PATH=/home/psana-build/btx/scripts:$PATH
-export PYTHONPATH=/home/psana2-build/lcls2/install/lib/python3.10/site-packages
+export PYTHONPATH=/home/psana2-build/lcls2/install/lib/python3.9/site-packages
 export PYTHONPATH=/home/psana2-build/btx:$PYTHONPATH
 
 exec "$@"
