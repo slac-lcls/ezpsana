754ca9b6d2b37348649db16872df94e202716d6c
fredericpoitevin <frederic.poitevin@gmail.com>
2022-11-07 13:39:16 -0800

    psana2nersc: try without creating environment

--- docker/psana2-nersc/entrypoint.sh
+++ docker/psana2-nersc/entrypoint.sh
@@ -4,7 +4,6 @@ unset LD_LIBRARY_PATH
 unset PYTHONPATH
 
 source /opt/anaconda3/etc/profile.d/conda.sh
-conda activate ana
 export PATH=/home/psana2-build/lcls2/install/bin:$PATH
 export PATH=/home/psana-build/btx/scripts:$PATH
 export PYTHONPATH=/home/psana2-build/lcls2/install/lib/python3.9/site-packages
