402a731a4cd7217dccf9c8a9486420e4535467b5
fredericpoitevin <frederic.poitevin@gmail.com>
2022-11-08 21:12:24 -0800

    adding ccp4 and XDS

--- docker/psana2-nersc/entrypoint.sh
+++ docker/psana2-nersc/entrypoint.sh
@@ -4,4 +4,7 @@ unset PYTHONPATH
 export PYTHONPATH=/home/psana2-build/lcls2/install/lib/python3.9/site-packages
 export PYTHONPATH=/home/psana2-build/btx:$PYTHONPATH
 
+mkdir -p $CCP4_SCR
+source /home/psana2-build/ccp4-7.1/bin/ccp4.setup-sh
+
 exec "$@"
