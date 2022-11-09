2e21a82d68e1e990e18c9113450cd0dbfa734ea1
fredericpoitevin <frederic.poitevin@gmail.com>
2022-11-08 18:38:14 -0800

    add entrypoint back to fix PYTHONPATH

--- docker/psana2-nersc/entrypoint.sh
+++ docker/psana2-nersc/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+unset PYTHONPATH
+export PYTHONPATH=/home/psana2-build/lcls2/install/lib/python3.9/site-packages
+export PYTHONPATH=/home/psana2-build/btx:$PYTHONPATH
+
 exec "$@"
