fdf09bb3bc0c206d205a67d34e4ca8a45e97a3ab
fredericpoitevin <frederic.poitevin@gmail.com>
2022-11-08 16:40:35 -0800

    cleaned up psana2-nersc Dockerfile: no entrypoint anymore. Added crystfel and mosflm.

--- docker/psana2-nersc/entrypoint.sh
+++ docker/psana2-nersc/entrypoint.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/env bash
 
-#unset LD_LIBRARY_PATH
-unset PYTHONPATH
-
-source /opt/anaconda3/etc/profile.d/conda.sh
-export PATH=/home/psana2-build/lcls2/install/bin:$PATH
-export PATH=/home/psana-build/btx/scripts:$PATH
-export PYTHONPATH=/home/psana2-build/lcls2/install/lib/python3.9/site-packages
-export PYTHONPATH=/home/psana2-build/btx:$PYTHONPATH
-
-export PS_SMD_CHUNKSIZE=64000000
-export HDF5_USE_FILE_LOCKING=FALSE
-
 exec "$@"
